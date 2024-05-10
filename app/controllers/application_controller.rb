0fc8125759f60e2acc02e7506bb7479481843125
ductung <nguyenductung224@gmail.com>
2024-05-10 17:59:45 +0700

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   before_action :set_locale
 
   private
@@ -11,5 +12,9 @@ class ApplicationController < ActionController::Base
     locale = params[:locale].to_s.strip.to_sym
     I18n.locale = I18n.available_locales.include?(locale) ?
       locale : I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
